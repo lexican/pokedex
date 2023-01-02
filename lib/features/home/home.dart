11e9fb9c7aff69b579132e98287d5e8b44cbd4e1
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-01-02 15:46:30 +0100

    feat : HomeTab

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,15 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/widgets/home/home_tab.dart';
 import 'package:pokedex/widgets/pokemon_app_bar/pokemon_app_bar.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      appBar: PokemonAppBar()
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  PageController? _pageViewController;
+  int _currentTabIndex = 0;
+
+  @override
+  void initState() {
+    _pageViewController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageViewController?.dispose();
+    super.dispose();
+  }
+
+  void _changePage(int pageNum) {
+    setState(() {
+      _currentTabIndex = pageNum;
+      _pageViewController?.animateToPage(
+        pageNum,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.ease,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const PokemonAppBar(),
+      body: DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            Container(
+              height: 52,
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  HomeTab(
+                    currentTabIndex: _currentTabIndex,
+                    tabIndex: 0,
+                    onTap: _changePage,
+                    tabLabel: 'All Pokemons',
+                  ),
+                  HomeTab(
+                    currentTabIndex: _currentTabIndex,
+                    tabIndex: 1,
+                    onTap: _changePage,
+                    tabLabel: 'Favourites',
+                    isFavourite: true,
+                    favouritesCount: 0,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                controller: _pageViewController,
+                onPageChanged: (value) {
+                  setState(() {
+                    _currentTabIndex = value;
+                  });
+                },
+                children: [
+                  Container(),
+                  Container(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/home/home_tab.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/widgets/pokemon_text/pokemon_text.dart';
+
+class HomeTab extends StatelessWidget {
+  final int tabIndex;
+  final int currentTabIndex;
+  final bool isFavourite;
+  final Function onTap;
+  final String tabLabel;
+  final int favouritesCount;
+  const HomeTab({
+    super.key,
+    required this.tabIndex,
+    required this.currentTabIndex,
+    this.isFavourite = false,
+    required this.onTap,
+    required this.tabLabel,
+    this.favouritesCount = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          onTap(tabIndex);
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    PokemanText.h1(
+                      content: tabLabel,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: currentTabIndex == tabIndex
+                          ? Theme.of(context).textTheme.headline1?.color
+                          : Theme.of(context).textTheme.bodyText1?.color,
+                    ),
+                    if (isFavourite)
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(6),
+                            child: PokemanText.b1(
+                              content: favouritesCount.toString(),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+            if (currentTabIndex == tabIndex)
+              Container(
+                height: 4,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF3558CD),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    topRight: Radius.circular(4),
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
