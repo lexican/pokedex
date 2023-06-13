4085eec5399568b55dc6feb3fb96d7cc7009e5ec
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-06-13 20:47:28 +0100

    feat : ApiResult

---
+++ lib/src/core/api/api_result.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pokedex/src/core/api/exceptions/dio_exceptions.dart';
+
+
+
+part 'api_result.freezed.dart';
+
+@freezed
+class ApiResult<T> with _$ApiResult<T> {
+  const factory ApiResult.success({required T data}) = Success<T>;
+
+  const factory ApiResult.failure({required DioExceptions error}) =
+      Failure<T>;
+}
